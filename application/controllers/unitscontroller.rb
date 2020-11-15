de05663debb6342231d43fc53264dfc19a2dc1c8
Kelin Field <kfield111@hotmail.com>
2020-11-15 15:35:57 +0000

    Added unit controller

---
+++ application/controllers/unitscontroller.rb
@@ -0,0 +1,11 @@
+class ArmiesController < AppController
+
+  get '/action_page.php' do
+    @unit = Unit.create(:unit_name => params[:unit_name])
+    user = current_user
+    @army =
+    @army.units << @unit
+    @unit.user = user
+    redirect "/armies/#{@army.id}"
+  end
+end
